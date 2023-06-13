263b601bcdf2fa69101ec2b400943686cf5ee1db
akula5 <niharikaak05@gmail.com>
2023-06-13 15:41:25 -0500

    exceptions

--- Project1/src/Day11/String2Int.java
+++ Project1/src/Day11/String2Int.java
@@ -3,22 +3,21 @@ package Project1.src.Day11;
 import java.util.*;
 
 public class String2Int {
-    public static int stringtoint(String s2) {
-        int number = Integer.parseInt(s2);
-        System.out.println(number);
-        return number;
-    }
-
-    public static void main(String[] args) {
-        try
-        {
-            stringtoint("23");
-            stringtoint("test");
-        }
-        catch (NumberFormatException e)
-        {
+    public static void stringtoint(String s2) {
+        try {
+            int number = Integer.parseInt(s2);
+            System.out.println(number);
+        } catch (NumberFormatException e) {
             System.out.println("Given String cannot be converted into Integer");
         }
+    }
+    public static void main(String[] args)
+    {
+
+        stringtoint("23");
+        stringtoint("45.67");
+        stringtoint("test");
+        stringtoint("21f");
 
     }
 }
